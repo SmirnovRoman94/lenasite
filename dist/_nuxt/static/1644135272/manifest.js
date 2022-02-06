8da7aac03b7d4dab8067d5a3a4e1b0808130c717
SmirnovRoman94 <smirnow3217831@gmail.com>
2022-02-06 11:43:37 +0300

    add

---
+++ dist/_nuxt/static/1644135272/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fadmin\u002Fblog\u002FnewPostBlog","\u002Fadmin\u002Fgoods\u002FnewGoods","\u002Fadmin\u002Fservices\u002FnewServices","\u002Fservices","\u002Fportfolio","\u002Fadmin\u002Fprices\u002FnewPrice","\u002Fpricing","\u002FCard","\u002Fhome","\u002Fblog","\u002Ftestimony","\u002Fcard"]})
